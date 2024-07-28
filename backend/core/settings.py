68c2f1f83e5e90b52eeb1813288a1d281ad0d4f8
kylych-dev <mirbekov1kylych@gmail.com>
2024-07-28 22:13:15 +0600

    create and connect postgresql db

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_db',
+        'USER': 'my_dbuser', 
+        'PASSWORD': '1',
+        'HOST': 'localhost',  
+        'PORT': '5432',           
     }
 }
 
